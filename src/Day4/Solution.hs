534e3045646eb5e0c8d6f6f3993fb36e995b75ed
ser <140543954+serlovesferrets@users.noreply.github.com>
2024-12-05 15:40:12 +0100

    done part 2 of day 4

--- src/Day4/Solution.hs
+++ src/Day4/Solution.hs
@@ -1,10 +1,12 @@
 module Day4.Solution where
 
+import Control.Applicative ((<|>))
 import Control.Monad (forM_)
-import Control.Monad.State (MonadIO (liftIO), StateT, modify, execStateT)
+import Control.Monad.State (MonadIO (liftIO), StateT, execStateT, modify)
 import Data.Array qualified as Arr
 import Data.Array.Base ((!), (!?))
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, isJust)
+import GHC.Base (when)
 import System.IO (readFile')
 
 fileName :: String
@@ -65,3 +67,54 @@ solution = execStateT solutionM 0
 
 -- >>> solution
 -- 2524
+
+check' :: Matrix Char -> Position -> Maybe ()
+check' matr start@(x, y) = do
+    'A' <- matr !!? start
+    conf_1 <|> conf_2 <|> conf_3 <|> conf_4
+  where
+    topRight = (x + 1, y + 1)
+    topLeft = (x - 1, y + 1)
+    btmLeft = (x - 1, y - 1)
+    btmRight = (x + 1, y - 1)
+
+    conf_1 = do
+        'M' <- matr !!? topLeft
+        'S' <- matr !!? btmRight
+        'M' <- matr !!? btmLeft
+        'S' <- matr !!? topRight
+        pure ()
+
+    conf_2 = do
+        'M' <- matr !!? topLeft
+        'S' <- matr !!? btmLeft
+        'M' <- matr !!? topRight
+        'S' <- matr !!? btmRight
+        pure ()
+
+    conf_3 = do
+        'M' <- matr !!? topRight
+        'S' <- matr !!? topLeft
+        'M' <- matr !!? btmRight
+        'S' <- matr !!? btmLeft
+        pure ()
+
+    conf_4 = do
+        'M' <- matr !!? btmLeft
+        'S' <- matr !!? topLeft
+        'M' <- matr !!? btmRight
+        'S' <- matr !!? topRight
+        pure ()
+
+solutionM' :: Stateful Int ()
+solutionM' = do
+    inputElts <- liftIO input
+    forM_ [0 .. length inputElts - 1] $ \i ->
+        forM_ [0 .. length (inputElts ! i) - 1] $ \j ->
+            when (isJust (check' inputElts (j, i))) $ modify (+ 1)
+
+solution' :: IO Int
+solution' = execStateT solutionM' 0
+
+-- >>> solution'
+-- 1873
